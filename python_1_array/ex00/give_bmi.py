25ca925f6c47a38f7a336ebdba1b21593f77ac6a
Jee <jeehyun24park@gmail.com>
2023-10-13 10:07:31 +0200

    python_1 - ex00

--- python_1_array/ex00/give_bmi.py
+++ python_1_array/ex00/give_bmi.py
@@ -5,7 +5,10 @@ def is_int_or_float(value) -> bool:
     return isinstance(value, (int, float))
 
 
-def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+def give_bmi(
+        height: list[int | float],
+        weight: list[int | float]
+) -> list[int | float]:
     """
     take 2 lists of integers or floats in input and returns a list
     of BMI values.
@@ -20,8 +23,10 @@ def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int |
         list[int | float]: a list of resulted BMI values
     """
     try:
-        assert len(height) == len(weight), "weight list and height list \
-            are not the same size"
+        assert len(height) != 0, "height list is empty"
+        assert len(weight) != 0, "weight list is empty"
+        assert len(height) == len(weight), \
+            "weight list and height list are not the same size"
         bmi_results = []
         for weight_value, height_value in zip(weight, height):
             assert is_int_or_float(weight_value), "value must be int or float"
@@ -34,10 +39,22 @@ def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int |
 
 
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    """
+    check if bmi values do not exceed the limit
+
+    Args:
+        bmi (list[int  |  float]): a list of bmi values
+        limit (int): the limit
+
+    Returns:
+        list[bool]: true if bmi value is above the limit
+    """
     try:
+        assert len(bmi) != 0, "bmi list is empty"
         for value in bmi:
             assert is_int_or_float(value), "value must be int or float"
-        assert isinstance(limit, int), "limit must be int"
+        assert limit >= 0 and isinstance(limit, int), "limit must be positive"
+        assert isinstance(limit, int), "limit must be a positive int"
         return list(map(lambda value: value <= limit, bmi))
     except AssertionError as assertion_error:
         print(f"AssertionError: {assertion_error}\n")
--- python_1_array/ex00/tester.py
+++ python_1_array/ex00/tester.py
@@ -11,8 +11,17 @@ def test_mandatory():
 
 
 class testGiveBmi(unittest.TestCase):
-    def test_size_mismatch(self):
+    height = [2.71, 1.15]
+    weight = [165.3, 38.4]
+
+    def test_empty_height_list(self):
+        self.assertEqual(give_bmi([1.89], []), None)
+        self.assertEqual(give_bmi([], [68.9]), None)
+        self.assertEqual(give_bmi([], []), None)
+
+    def test_size_list_mismatch(self):
         self.assertEqual(give_bmi([1.78], [2.98, 32.31]), None)
+        self.assertEqual(give_bmi([1.78, 1.49, 2.01], [2.98, 32.31]), None)
 
     def test_invalid_height(self):
         self.assertEqual(give_bmi(["toto"], [50.2]), None)
@@ -20,8 +29,22 @@ class testGiveBmi(unittest.TestCase):
     def test_invalid_weight(self):
         self.assertEqual(give_bmi([1.64], ["tutu"]), None)
 
+    def test_valid_weight_and_height_lists(self):
+        self.assertEqual(give_bmi(self.height, self.weight),
+                         [22.507863455018317, 29.0359168241966])
+
+
+class testApplyLimit(unittest.TestCase):
+    def test_empty_bmi_list(self):
+        self.assertEqual(apply_limit([], 3), None)
+
+    def test_negative_limit_value(self):
+        self.assertEqual(apply_limit([2.32], -32), None)
+
+    def test_float_limit_value(self):
+        self.assertEqual(apply_limit([2.32], 32.01), None)
+
 
 if __name__ == "__main__":
     test_mandatory()
     unittest.main()
-
