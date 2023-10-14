a9966030e3410ec778fcaa89fec26f8ee86175f8
Jee hyun PARK <jeepark@made-f0Br8s17.clusters.42paris.fr>
2023-10-14 17:19:20 +0200

    python_1 - ex05 - clean

--- python_1_array/ex05/pimp_image.py
+++ python_1_array/ex05/pimp_image.py
@@ -2,89 +2,90 @@ from numpy import ndarray, empty_like, round
 
 
 def ft_invert(array: ndarray) -> ndarray:
-    print(type(array))
-
-    # Get the shape of the 3D array
-    depth, rows, cols = array.shape
+    """Inverts the color of the image received.
+    """
+    rows, cols, depth = array.shape
     result = empty_like(array)
-    print(f"dephth: {depth}")
-    # Iterate through the 3D array using nested loops
-    for d in range(depth):
-        for r in range(rows):
-            for c in range(cols):
-                value = array[d, r, c]
-                # print(f"Element at depth {d}, row {r}, column {c} is {value}")
-                result[d, r, c] = 255 - value
-
+    for r in range(rows):
+        for c in range(cols):
+            for d in range(depth):
+                value = array[r, c, d]
+                result[r, c, d] = 255 - value
+    print(f"The shape of image is: {result.shape}\n{result}")
     return result
 
 
 # #your code here
 def ft_red(array: ndarray) -> ndarray:
-    rows, cols, depth = array.shape
+    """Fiters the green and blue colors of the image received
+to render a red image.
+    """
     result = empty_like(array)
-    print(f"depth:{depth}, rows:{rows}, cols:{cols}")
-  # Iterate through the 3D array using nested loops
     for i in range(array.shape[0]):
         for j in range(array.shape[1]):
-            # Set the red color channel in the result to the original value
+            # R
             result[i, j, 0] = array[i, j, 0]
-            # Set the green and blue channels in the result to 0 to remove them
+            # G
             result[i, j, 1] *= 0
+            # B
             result[i, j, 2] *= 0
-
+    print(f"The shape of image is: {result.shape}\n{result}")
     return result
 
 
 # #your code here
 def ft_green(array: ndarray) -> ndarray:
-    rows, cols, depth = array.shape
+    """Fiters the red and blue colors of the image received
+to render a green image.
+    """
     result = empty_like(array)
-    print(f"depth:{depth}, rows:{rows}, cols:{cols}")
-  # Iterate through the 3D array using nested loops
     for i in range(array.shape[0]):
         for j in range(array.shape[1]):
-            # Set the red color channel in the result to the original value
+            # R
             result[i, j, 0] = array[i, j, 0] - array[i, j, 0]
-            # Set the green and blue channels in the result to 0 to remove them
+            # G
             result[i, j, 1] = array[i, j, 1]
+            # B
             result[i, j, 2] = array[i, j, 2] - array[i, j, 2]
-
+    print(f"The shape of image is: {result.shape}\n{result}")
     return result
 
 
 # #your code here
 def ft_blue(array: ndarray) -> ndarray:
-    rows, cols, depth = array.shape
+    """Fiters the red and green colors of the image received
+to render a blue image.
+    """
     result = empty_like(array)
-    print(f"depth:{depth}, rows:{rows}, cols:{cols}")
-  # Iterate through the 3D array using nested loops
     for i in range(array.shape[0]):
         for j in range(array.shape[1]):
-            # Set the red color channel in the result to the original value
+            # R
             result[i, j, 0] = 0
-            # Set the green and blue channels in the result to 0 to remove them
+            # G
             result[i, j, 1] = 0
+            # B
             result[i, j, 2] = array[i, j, 2]
-
+    print(f"The shape of image is: {result.shape}\n{result}")
     return result
 # #your code here
 
 
 def ft_grey(array: ndarray) -> ndarray:
-    rows, cols, depth = array.shape
+    """Lower intensity of each color space to render
+a grayscaled image.
+    """
     result = empty_like(array)
     result = result.astype('float')
-    print(f"depth:{depth}, rows:{rows}, cols:{cols}")
-  # Iterate through the 3D array using nested loops
     for i in range(array.shape[0]):
         for j in range(array.shape[1]):
-           # Calculate the grayscale intensity as the average of the color channels
             intensity = (array[i, j, 0] / 02.99 + array[i, j,
                          1] / 05.87 + array[i, j, 2] / 01.14) / 1.2
+            # R
             result[i, j, 0] = intensity
+            # G
             result[i, j, 1] = intensity
+            # B
             result[i, j, 2] = intensity
     result = round(result).astype('int')
-
+    print(f"The shape of image is: {result.shape}\n{result}")
     return result
--- python_1_array/ex05/tester.py
+++ python_1_array/ex05/tester.py
@@ -1,26 +1,43 @@
 from load_image import ft_load
 from pimp_image import ft_invert, ft_red, ft_green, ft_blue, ft_grey
-import matplotlib.pyplot as plt
-
+from matplotlib.pyplot import show, figure
 from mpl_toolkits.axes_grid1 import ImageGrid
-array = ft_load("landscape.jpg")
 
-print(type(array))
-inverted_color = ft_invert(array)
-red_filtered = ft_red(array)
-green_filtered = ft_green(array)
-blue_filtered = ft_blue(array)
-grey_filtered = ft_grey(array)
-# print(ft_invert.__doc__)
 
-# plt.imshow(inverted_color)
-fig = plt.figure(figsize=(10., 10.))
-grid = ImageGrid(fig, 111,
-                 nrows_ncols=(3, 2),
-                 axes_pad=0.2,
-                 )
+def main():
+    """testing colors masks
+    """
+    try:
+        array = ft_load("landscape.jpg")
+    except Exception as error:
+        print(f"An error occurred: {error}")
+        exit(1)
+    array = ft_load("landscape.jpg")
+    inverted_color = ft_invert(array)
+    red_filtered = ft_red(array)
+    green_filtered = ft_green(array)
+    blue_filtered = ft_blue(array)
+    grey_filtered = ft_grey(array)
 
-for ax, im in zip(grid, [array, inverted_color, red_filtered, green_filtered, blue_filtered, grey_filtered]):
-    ax.imshow(im)
+    print(ft_invert.__doc__)
+    print(ft_red.__doc__)
+    print(ft_blue.__doc__)
+    print(ft_green.__doc__)
+    print(ft_grey.__doc__)
 
-plt.show()
+    fig = figure(figsize=(10., 10.))
+    grid = ImageGrid(fig, 111,
+                     nrows_ncols=(3, 2),
+                     axes_pad=0.2,
+                     )
+
+    for ax, im in zip(grid,
+                      [array, inverted_color, red_filtered,
+                       green_filtered, blue_filtered, grey_filtered]):
+        ax.imshow(im)
+
+    show()
+
+
+if __name__ == "__main__":
+    main()
