c88c75ee0e0c4d00ab8ac00ace9aa8c7d98fc65b
Jee hyun PARK <jeepark@made-f0Br8s17.clusters.42paris.fr>
2023-10-14 15:27:02 +0200

    python_1 - ex01 - clean

--- python_1_array/ex01/array2D.py
+++ python_1_array/ex01/array2D.py
@@ -1,4 +1,4 @@
-import numpy as np
+from numpy import shape
 
 
 def slice_me(family: list, start: int, end: int) -> list:
@@ -15,15 +15,16 @@ def slice_me(family: list, start: int, end: int) -> list:
     """
     try:
         assert type(family) is list, "sequence is not a list"
+        assert len(family) != 0, "list is empty"
         assert all([type(row) is list for row in family]), \
             "subsequent sequences are not all lists"
         assert all([len(row) == len(family[0]) for row in family]), \
             "subsequent lists are not the same size"
 
-        print(f"My shape is : {np.shape(family)}")
+        print(f"My shape is : {shape(family)}")
         sliced_family = family[start:end]
-        print(f"My new shape is : {np.shape(sliced_family)}")
+        print(f"My new shape is : {shape(sliced_family)}")
         return sliced_family
     except AssertionError as assertion_error:
         print(f"AssertionError:{assertion_error}")
-        
+        return None
--- python_1_array/ex01/tester.py
+++ python_1_array/ex01/tester.py
@@ -1,8 +1,10 @@
 from array2D import slice_me
-import unittest
+from unittest import TestCase, main
 
 
 def test_mandatory():
+    """mandatory tests given by the subject
+    """
     family = [[1.80, 78.4],
               [2.15, 102.7],
               [2.10, 98.5],
@@ -11,16 +13,44 @@ def test_mandatory():
     print(slice_me(family, 1, -2))
 
 
-class testSliceMe(unittest.TestCase):
+class testSliceMe(TestCase):
+    def test_list_empty(self):
+        """test with empty lists
+        """
+        family = []
+        self.assertEqual(slice_me(family, 0, 1), None)
+
     def test_list_size(self):
-        print("test_list_size")
+        """test if subsequent lists are not the same size
+        """
         family = [[1.80], [2.15, 102.7]]
         self.assertEqual(slice_me(family, 0, 1), None)
-    
+
     def test_not_a_list(self):
-        family = {}
+        """test if list is type of list
+        """
+        family = {1, 2, 3}
         self.assertEqual(slice_me(family, 0, 1), None)
+        family = (1, 2, 3)
+        self.assertEqual(slice_me(family, 0, 1), None)
+        family = [1, 2, 3]
+        self.assertEqual(slice_me(family, 0, 1), None)
+
+    def test_valid_list(self):
+        """test with valid arguments
+        """
+        family = [[1], [2], [3]]
+        self.assertEqual(slice_me(family, 0, 1), [[1]])
+        family = [[1.80, 78.4],
+                  [2.15, 102.7],
+                  [2.10, 98.5],
+                  [1.88, 75.2]]
+        self.assertEqual(slice_me(family, 0, 2), [[1.8, 78.4], [2.15, 102.7]])
+        self.assertEqual(slice_me(family, 1, -2), [[2.15, 102.7]])
+        family = [["jee"], ["hyun"]]
+        self.assertEqual(slice_me(family, 0, 1), [["jee"]])
+
 
 if __name__ == "__main__":
     test_mandatory()
-    unittest.main()
+    main()
