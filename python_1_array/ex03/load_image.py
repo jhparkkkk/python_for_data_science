31183611995fe7ad31b02e40e8359a527c2a9d1e
Jee hyun PARK <jeepark@made-f0Br8s17.clusters.42paris.fr>
2023-10-14 17:19:50 +0200

    ok to submit

--- python_1_array/ex03/load_image.py
+++ python_1_array/ex03/load_image.py
@@ -4,6 +4,15 @@ from pathlib import Path
 
 
 def ft_load(path: str) -> list:
+    """ loads an image, prints its format, and its pixels
+content in RGB format.
+
+    Args:
+        path (str): path of the image file
+
+    Returns:
+        list (int): pixels values in RGB format
+    """
     try:
         # check if file exist
         path = Path(path)
@@ -18,4 +27,3 @@ def ft_load(path: str) -> list:
         raise (error)
     except PermissionError as error:
         raise (error)
-
--- python_1_array/ex03/zoom.py
+++ python_1_array/ex03/zoom.py
@@ -1,31 +1,40 @@
-import matplotlib.pyplot as plt
 from load_image import ft_load
-import cv2
-import numpy as np
+from cv2 import cvtColor, COLOR_BGR2GRAY
+from numpy import newaxis
+from matplotlib.pyplot import imshow, show
 
 
 def main():
+    """
+    crop an image to zoom in.
+    Display information about the cropped image:
+    - The size in pixel on both X and Y axis
+    - The number of channel
+    - The pixel content of the image.
+    - Display the scale on the x and y axis on the image
+    """
     try:
         # Load image
         image = ft_load("animal.jpeg")
         print(image)
-    except FileNotFoundError as error:
-        print(error)
+    except Exception as error:
+        print(f"An error occurred: {error}")
         exit(1)
-    # Convert the color image to grayscale
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Convert the chromatic image to grayscale
+    gray_image = cvtColor(image, COLOR_BGR2GRAY)
     # slice
     start_x = 100
     start_y = 450
     end_x = start_x + 400
     end_y = start_y + 400
     # Crop the image
-    crop_img = gray_image[start_x:end_x, start_y:end_y, np.newaxis]
+    crop_img = gray_image[start_x:end_x, start_y:end_y, newaxis]
     print(f"New shape after slicing:{crop_img.shape} or {crop_img.shape[:2]}")
     print(crop_img)
-    # Display the grayscale image
-    plt.imshow(crop_img, cmap="gray")
-    plt.show()
+    # Display the image
+    imshow(crop_img, cmap="gray")
+    show()
 
 
 if __name__ == "__main__":
