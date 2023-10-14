7d35c28b3f225eab06ad10b49ef4299b7dd5363d
Jee hyun PARK <jeepark@made-f0Br8s17.clusters.42paris.fr>
2023-10-14 16:34:45 +0200

    python_1 - ex04 - clean

--- python_1_array/ex04/rotate.py
+++ python_1_array/ex04/rotate.py
@@ -1,100 +1,69 @@
-import matplotlib.pyplot as plt
 from load_image import ft_load
-import cv2
-import numpy as np
+from cv2 import cvtColor, COLOR_BGR2GRAY
+from numpy import newaxis, zeros, indices, round, abs
 from math import radians, cos, sin
-# def rotate_image(image, angle_degrees):
-#     angle = radians(angle_degrees)
-#     height = image.shape[0]
-#     width = image.shape[1]
+from matplotlib.pyplot import imshow, show
 
 
-#     # Calculate the dimensions of the new image
-#      # Calculate the dimensions of the new image
-#     new_width = 400
-#     new_height = 400
-
-
-#     # Create an empty canvas for the rotated image
-#     rotated_image = np.zeros((new_height, new_width), dtype=image.dtype)
-
-#     # Calculate the center of the original image
-#     center_x, center_y = width // 2, height // 2
-
-#     # Calculate the center of the rotated image
-#     new_center_x, new_center_y = new_width // 2, new_height // 2
-
-#     for i in range(new_height):
-#         for j in range(new_width):
-#             # Calculate the original coordinates
-#             x = (i - new_center_y) * cos(angle) + (j - new_center_x) * sin(angle) + center_x
-#             y = -(i - new_center_y) * sin(angle) + (j - new_center_x) * cos(angle) + center_y
-            
-#             if 0 <= x < height and 0 <= y < width:
-#                 rotated_image[i, j] = image[int(x), int(y)]
-
-#     return rotated_image
 def main():
+    """
+    Crop an image and then transpose it by 90 degrees.
+Display the new image and print new image's data : shape and values
+    """
     try:
         # Load image
         image = ft_load("animal.jpeg")
-    except FileNotFoundError as error:
-        print(error)
+        print(image)
+    except Exception as error:
+        print(f"An error occurred: {error}")
         exit(1)
-    # Convert the color image to grayscale
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Convert the chromatic image to grayscale
+    gray_image = cvtColor(image, COLOR_BGR2GRAY)
     # slice
     start_x = 100
     start_y = 450
     end_x = start_x + 400
     end_y = start_y + 400
     # Crop the image
-    crop_img = gray_image[start_x:end_x, start_y:end_y, np.newaxis]
-    print(f"The shape of image is: {crop_img.shape} or {crop_img.shape[:2]}")
+    crop_img = gray_image[start_x:end_x, start_y:end_y, newaxis]
+    print(f"New shape after slicing:{crop_img.shape} or {crop_img.shape[:2]}")
     print(crop_img)
-    # Transpose the 
-    # rot_img = np.zeros(crop_img.shape[:2], dtype=np.float32)
-    # rot_img = rotate_image(crop_img, 90)
-    # rot_img = np.uint8(rot_img)
-    # First we will convert the degrees into radians
-    rads = radians(90)
-
-    # We consider the rotated image to be of the same size as the original
-    rot_img = np.zeros(crop_img.shape[:2])
+    # Transpose
+    rads = radians(-90)
+    # transposed image is here the same size as crop_img
+    rot_img = zeros(crop_img.shape[:2])
 
     # Finding the center point of rotated (or original) image.
     height = rot_img.shape[0]
-    width  = rot_img.shape[1]
-    print('height:', height)
-    print('width:', width)
+    width = rot_img.shape[1]
 
+    midx, midy = (width//2, height//2)
+    # Create arrays of indices
+    i, j = indices(rot_img.shape[:2])
 
-    midx,midy = (width//2, height//2)
-    print(f"midx:", midx)
-    i = 0
-    j = 0
-    for i in range(height):
-        
-        for j in range(width):
-            x = (i-midx)*abs(cos(rads))+(j-midy)*abs(sin(rads))
-            y = -(i-midx)*abs(sin(rads))+(j-midy)*abs(cos(rads))
+    # Calculate the new indices (after transpose)
+    # rotation
+    x = (i - midx) * abs(cos(rads)) + (j - midy) * abs(sin(rads))
+    y = -(i - midx) * abs(sin(rads)) + (j - midy) * abs(cos(rads))
+    # translation
+    x = round(x).astype(int) + midx
+    y = round(y).astype(int) + midy
 
-            x = round(x)+midx 
-            y = round(y)+midy
+    # check if x and y are inside array
+    valid_indices = (x >= 0) & (y >= 0) & (x < height) & (y < width)
 
+    # Flatten indices -> 2d to 1d
+    flat_indices = x[valid_indices] * width + y[valid_indices]
 
-            if (x>=0 and y>=0 and x<=height  and  y<width):
-                rot_img[i,j] = crop_img[x,y]
-            else:
-                print(f"{i} and {j} | {x} and {y} ")
-                rot_img[i,j] = 0
-    
+    # Assign values with flattend indices
+    rot_img[i[valid_indices], j[valid_indices]] = crop_img.ravel()[
+        flat_indices]
+
     print(f"New shape after Transpose: {rot_img.shape}\n{rot_img}")
 
-
     # Display the image
-    plt.imshow(rot_img, cmap="gray")
-    plt.show()
+    imshow(rot_img, cmap="gray")
+    show()
 
 
 if __name__ == "__main__":
