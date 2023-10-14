dc3a1757b902b3dc8f6cebec10d47eb7d28aa8a7
Jee <jeehyun24park@gmail.com>
2023-10-14 11:07:00 +0200

    python_1 - ex05

--- python_1_array/ex05/pimp_image.py
+++ python_1_array/ex05/pimp_image.py
@@ -1,4 +1,4 @@
-from numpy import ndarray, empty_like
+from numpy import ndarray, empty_like, round
 def ft_invert(array: ndarray) -> ndarray:
     print(type(array))
 
@@ -18,10 +18,68 @@ def ft_invert(array: ndarray) -> ndarray:
 
 
 # #your code here
-# def ft_red(array) -> array:
+def ft_red(array: ndarray) -> ndarray:
+    rows, cols, depth = array.shape
+    result = empty_like(array)
+    print(f"depth:{depth}, rows:{rows}, cols:{cols}")
+  # Iterate through the 3D array using nested loops
+    for i in range(array.shape[0]):
+        for j in range(array.shape[1]):
+            # Set the red color channel in the result to the original value
+            result[i, j, 0] = array[i, j, 0]
+            # Set the green and blue channels in the result to 0 to remove them
+            result[i, j, 1] *= 0
+            result[i, j, 2] *= 0
+    
+    return result
+
+
 # #your code here
-# def ft_green(array) -> array:
+def ft_green(array: ndarray) -> ndarray:
+    rows, cols, depth = array.shape
+    result = empty_like(array)
+    print(f"depth:{depth}, rows:{rows}, cols:{cols}")
+  # Iterate through the 3D array using nested loops
+    for i in range(array.shape[0]):
+        for j in range(array.shape[1]):
+            # Set the red color channel in the result to the original value
+            result[i, j, 0] = array[i, j, 0] - array[i, j, 0]
+            # Set the green and blue channels in the result to 0 to remove them
+            result[i, j, 1] = array[i, j, 1]
+            result[i, j, 2] = array[i, j, 2] - array[i, j, 2]
+    
+    return result
+
+
 # #your code here
-# def ft_blue(array) -> array:
+def ft_blue(array: ndarray) -> ndarray:
+    rows, cols, depth = array.shape
+    result = empty_like(array)
+    print(f"depth:{depth}, rows:{rows}, cols:{cols}")
+  # Iterate through the 3D array using nested loops
+    for i in range(array.shape[0]):
+        for j in range(array.shape[1]):
+            # Set the red color channel in the result to the original value
+            result[i, j, 0] = 0
+            # Set the green and blue channels in the result to 0 to remove them
+            result[i, j, 1] = 0
+            result[i, j, 2] = array[i, j, 2] 
+    
+    return result
 # #your code here
-# def ft_grey(array) -> array
+def ft_grey(array: ndarray) -> ndarray:
+    rows, cols, depth = array.shape
+    result = empty_like(array)
+    result = result.astype('float') 
+    print(f"depth:{depth}, rows:{rows}, cols:{cols}")
+  # Iterate through the 3D array using nested loops
+    for i in range(array.shape[0]):
+        for j in range(array.shape[1]):
+           # Calculate the grayscale intensity as the average of the color channels
+            intensity = (array[i, j, 0] / 02.99 + array[i, j, 1] / 05.87 + array[i, j, 2] / 01.14) / 1.2
+            result[i, j, 0] = intensity
+            result[i, j, 1] = intensity
+            result[i, j, 2] = intensity
+    result = round(result).astype('int')
+
+    return result
--- python_1_array/ex05/tester.py
+++ python_1_array/ex05/tester.py
@@ -1,16 +1,18 @@
 from load_image import ft_load
-from pimp_image import ft_invert
+from pimp_image import ft_invert, ft_red, ft_green, ft_blue, ft_grey
 import matplotlib.pyplot as plt
 ...
 array = ft_load("landscape.jpg")
 
 print(type(array))
-inverted_color = ft_invert(array)
-# ft_red(array)
-# ft_green(array)
-# ft_blue(array)
-# ft_grey(array)
+# inverted_color = ft_invert(array)
+# red_filtered = ft_red(array)
+# green_filtered = ft_green(array)
+# blue_filtered = ft_blue(array)
+grey_filtered = ft_grey(array)
 # print(ft_invert.__doc__)
 
-plt.imshow(inverted_color)
+# plt.imshow(inverted_color)
+plt.imshow(grey_filtered)  # Use "gray" colormap for grayscale images
+
 plt.show()
