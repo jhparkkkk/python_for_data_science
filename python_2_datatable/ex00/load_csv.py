beb3baa4c43e2cda836582badcdfca64371fed6d
Jee hyun PARK <jeepark@made-f0Dr6s11.clusters.42paris.fr>
2023-10-17 19:38:58 +0200

    python_2 - ex00 - clean

--- python_2_datatable/ex00/load_csv.py
+++ python_2_datatable/ex00/load_csv.py
@@ -1,13 +1,26 @@
-import pathlib as pathlib
 import pandas as pd
 
+
 def load(path: str) -> pd.core.frame.DataFrame:
+    """Loads data from .csv file writes the dimensions of the data set
+and returns it
+
+    Args:
+        path (str): .csv file
+    Returns:
+        pd.core.frame.DataFrame: pandas dataframe
+    """
     try:
-        # load .csv as dataframe 
+        # load .csv as dataframe
+        assert path.endswith('.csv'), "not a csv file"
         dataframe = pd.read_csv(path)
-        print(dataframe.shape)
+        print(f"Loading dataset of dimensions {dataframe.shape}")
         return dataframe
+    except AssertionError as error:
+        print(f"Invalid file: {error}")
     except Exception as error:
         print(f"CSV file not valid: {error}")
+        return None
     except FileNotFoundError as error:
         print(error)
+        return None
--- python_2_datatable/ex00/tester.py
+++ python_2_datatable/ex00/tester.py
@@ -1,5 +1,23 @@
 from load_csv import load
+from unittest import TestCase, main
 
-print(load("life_expectancy_years.csv"))
-print(load("population_total.csv"))
-print(load("income_per_person_gdppercapita_ppp_inflation_adjusted.csv"))
+
+def test_mandatory():
+    print(load("life_expectancy_years.csv"))
+
+
+class testLoad(TestCase):
+    def test_invalid_path(self):
+        """test with invalid path
+        """
+        self.assertEqual(load(""), None)
+        self.assertEqual(load("File/does/not.exist"), None)
+        self.assertEqual(load(" "), None)
+        self.assertEqual(load(""), None)
+        self.assertEqual(load("2131"), None)
+        self.assertEqual(load("bad.txt"), None)
+
+
+if __name__ == "__main__":
+    test_mandatory()
+    main()
